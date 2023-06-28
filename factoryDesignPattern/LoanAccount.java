15df5ffff5f1ab67e43bc6b4a5346ac7818004ed
Akshata-71 <107490595+Akshata-71@users.noreply.github.com>
2023-06-28 11:07:37 +0530

    Add files via upload
    Creating object of implementation layer without exposing it to the utilization layer is called as FACTORY DESIGN PATTERN.
    Provides high security to application.

---
+++ factoryDesignPattern/LoanAccount.java
@@ -0,0 +1,24 @@
+package factoryDesignPattern;
+
+public class LoanAccount implements Bank{
+    private double balance ;
+
+    @Override
+    public void deposit(double amt) {
+        balance -= amt;
+        System.out.println(amt + " DEPOSIT SUCCESSFULLY !!");
+    }
+
+    @Override
+    public void withdraw(double amt) {
+
+        balance += amt;
+        System.out.println(amt + " LOAN PASSED SUCCESSFULLY !!");
+
+    }
+
+    @Override
+    public void displayBalance() {
+        System.out.println("THE LOAN AMOUNT IS "+ balance);
+    }
+}
